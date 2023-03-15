5907d3766b9ff93b7b23a99cf14e9850de29a129
Tianhao Zhang <zhangth1990@hotmail.com>
2023-03-14 20:14:07 -0600

    rigidbody & adding force with physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Animator anim;
+    [SerializeField]
+    private float jumpForce = 100f;
+
+    private Animator anim;
+
+    private Rigidbody rigidbody;
+
+    private bool jump = false;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -17,6 +25,20 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             anim.Play("Jump");
+            rigidbody.useGravity = true;
+            jump = true;
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (jump == true)
+        {
+            jump = false;
+            rigidbody.velocity = new Vector2(0, 0);
+            rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode.Impulse);
+        }
+
+        print(rigidbody.velocity.y);
     }
 }
